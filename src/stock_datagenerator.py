b856edbd8ec4f404c585eba8e83935d83f7e4cee
Cameron Gordon <camerong888@gmail.com>
2023-11-17 00:23:43 -0500

    First implementation of LSTM

--- src/stock_datagenerator.py
+++ src/stock_datagenerator.py
@@ -1,26 +1,32 @@
 import yfinance as yf
 import pandas as pd
+import pandas_ta as ta
 import os
 
-startDate = '2021-01-01'
-endDate = '2023-11-01'
+startDate = "2021-01-01"
+endDate = "2023-11-01"
+
 
 def get_sp500_tickers():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     table = pd.read_html(url, header=0)[0]  # Read the first table found
-    tickers = table['Symbol'].tolist()
-    tickers.append('^GSPC') #add in S&P500
-    tickers = [ticker.replace('.', '-') for ticker in tickers]
+    tickers = table["Symbol"].tolist()
+    tickers.append("^GSPC")  # add in S&P500
+    tickers = [ticker.replace(".", "-") for ticker in tickers]
     return tickers
+
 
 def download_data(tickers, start_date, end_date):
     failed_tickers = []
 
     for ticker in tickers:
         try:
-            ticker_data = yf.download(ticker, start=start_date, end=end_date)            
-            save_data(ticker_data, f"{ticker}.csv", )
-        
+            ticker_data = yf.download(ticker, start=start_date, end=end_date)
+            save_data(
+                ticker_data,
+                f"{ticker}.csv",
+            )
+
         except Exception as e:
             print(f"Failed to download {ticker}: {e}")
             failed_tickers.append(ticker)
@@ -28,7 +34,8 @@ def download_data(tickers, start_date, end_date):
     if len(failed_tickers) > 0:
         print("Failed downloads:", failed_tickers)
 
-def save_data(df, filename, directory='data/stocks'):
+
+def save_data(df, filename, directory="data/stocks"):
     """
     Saves the dataframe to a specified folder as a CSV file.
     :param df: DataFrame to save
@@ -36,20 +43,45 @@ def save_data(df, filename, directory='data/stocks'):
     :param folder: Folder to save the file in
     """
     # Construct file path
-    base_dir = os.path.dirname(__file__)  # gets the directory where the script is located
-    file_path = os.path.join(base_dir, '..', directory, filename)
+    base_dir = os.path.dirname(
+        __file__
+    )  # gets the directory where the script is located
+    file_path = os.path.join(base_dir, "..", directory, filename)
 
     # Create folder if it doesn't exist
-    if not os.path.exists(os.path.join(base_dir, '..', directory)):
-        os.makedirs(os.path.join(base_dir, '..', directory))
+    if not os.path.exists(os.path.join(base_dir, "..", directory)):
+        os.makedirs(os.path.join(base_dir, "..", directory))
 
     # Save the file
     df.to_csv(file_path)
 
+
 # Fetch data
-top500_stock_tickers = get_sp500_tickers()
-download_data(top500_stock_tickers, startDate, endDate)
+# top500_stock_tickers = get_sp500_tickers()
+# download_data(top500_stock_tickers, startDate, endDate)
 
+# Generate indicator data
 
-#recurrent neural network takes trends and more recent days into account
+# Adding indicators
+snp500_data = yf.download(tickers="^GSPC", start=startDate, end=endDate)
+snp500_data["RSI"] = ta.rsi(snp500_data.Close, length=15)
+snp500_data["EMAF"] = ta.ema(snp500_data.Close, length=20)
+snp500_data["EMAM"] = ta.ema(snp500_data.Close, length=100)
+snp500_data["EMAS"] = ta.ema(snp500_data.Close, length=150)
 
+snp500_data["Target"] = snp500_data["Adj Close"] - snp500_data.Open
+snp500_data["Target"] = snp500_data["Target"].shift(-1)
+
+snp500_data["TargetClass"] = [
+    1 if snp500_data.Target[i] > 0 else 0 for i in range(len(snp500_data))
+]
+
+snp500_data["TargetNextClose"] = snp500_data["Adj Close"].shift(-1)
+
+snp500_data.dropna(inplace=True)
+snp500_data.reset_index(inplace=True)
+snp500_data.drop(["Volume", "Close", "Date"], axis=1, inplace=True)
+
+snp500_data_set = snp500_data.iloc[:, 0:11]  # .values
+
+save_data(snp500_data_set, "snp500_data_set.csv", directory="data/indicators")
--- src/train.py
+++ src/train.py
@@ -2,9 +2,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
+import keras
+from keras import optimizers
+from keras.callbacks import History
+from keras.models import Model, Sequential
+from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate, TimeDistributed
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
+# Hyperparameters
+backcandles = 30
+
+
+np.random.seed(10)
+
+
+file_path = '../data/indicators/snp500_data_set.csv'
+snp500_data_set = pd.read_csv(file_path)
+pd.set_option('display.max_columns', None)
+
+sc = MinMaxScaler(feature_range=(0,1))
+data_set_scaled = sc.fit_transform(snp500_data_set)
+#print(data_set_scaled)
+
+# multiple feature from data provided to the model
+X = []
+#print(data_set_scaled[0].size)
+#data_set_scaled=data_set.values
+#print(data_set_scaled.shape[0])
+for j in range(8):#data_set_scaled[0].size):#2 columns are target not X
+    X.append([])
+    for i in range(backcandles, data_set_scaled.shape[0]):#backcandles+2
+        X[j].append(data_set_scaled[i-backcandles:i, j])
+
+#move axis from 0 to position 2
+X=np.moveaxis(X, [0], [2])
+
+#Erase first elements of y because of backcandles to match X length
+#del(yi[0:backcandles])
+#X, yi = np.array(X), np.array(yi)
+# Choose -1 for last column, classification else -2...
+X, yi =np.array(X), np.array(data_set_scaled[backcandles:,-1])
+y=np.reshape(yi,(len(yi),1))
+#y=sc.fit_transform(yi)
+#X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+# print(X)
+# print(X.shape)
+# print(y)
+# print(y.shape)
+
+# split data into train test sets
+splitlimit = int(len(X)*0.8)
+# print(splitlimit)
+X_train, X_test = X[:splitlimit], X[splitlimit:]
+y_train, y_test = y[:splitlimit], y[splitlimit:]
+# print(X_train.shape)
+# print(X_test.shape)
+# print(y_train.shape)
+# print(y_test.shape)
+# print(y_train)
+
+lstm_input = Input(shape=(backcandles, 8), name='lstm_input')
+inputs = LSTM(150, name='first_layer')(lstm_input)
+inputs = Dense(1, name='dense_layer')(inputs)
+output = Activation('linear', name='output')(inputs)
+model = Model(inputs=lstm_input, outputs=output)
+adam = optimizers.Adam()
+model.compile(optimizer=adam, loss='mse')
+model.fit(x=X_train, y=y_train, batch_size=15, epochs=30, shuffle=True, validation_split = 0.1)
+
+y_pred = model.predict(X_test)
+#y_pred=np.where(y_pred > 0.43, 1,0)
+for i in range(10):
+    print(y_pred[i], y_test[i])
+
+plt.figure(figsize=(16,8))
+plt.plot(y_test, color = 'black', label = 'Test')
+plt.plot(y_pred, color = 'green', label = 'Prediction')
+plt.legend()
+plt.show()
